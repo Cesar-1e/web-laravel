52745183c24c3ce7c29fba36ea4bbe0eaeca793b
Cesar 1e <cesar.augusto9782@gmail.com>
2025-10-10 19:31:52 -0500

    Creación de formularios para responder en foros con Laravel

---
+++ app/Http/Controllers/AnswerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class AnswerController extends Controller
+{
+    public function store(Request $request, Question $question){
+        $request->validate([
+            "content" => "required|string|max:1900"
+        ]);
+
+        $question->answers()->create([
+            "content" => $request->content,
+            "user_id" => 10,
+        ]);
+
+        return back();
+    }
+}
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -20,7 +20,7 @@
                     <a href="#" class="text-xs font-semibold hover:underline">
                         Edit
                     </a>
-                    
+
                     <form action="#" onsubmit="return confirm('¿Estás seguro de eliminar esta pregunta?');">
                         @csrf
                         @method('DELETE')
@@ -40,7 +40,7 @@
 
         <liveware:comment :commentable="$question" />
     </div>
-    
+
     <ul class="space-y-4">
         @foreach ($question->answers as $answer)
         <li>
@@ -52,15 +52,31 @@
                         {{ $answer->content }}
                     </p>
                     <p class="text-xs text-gray-500">
-                        {{ $answer->user->name }} | 
+                        {{ $answer->user->name }} |
                         {{ $answer->created_at->diffForHumans() }}
                     </p>
-                    
+
                     <livewire:comment :commentable="$answer" />
                 </div>
-            </div>  
+            </div>
         </li>
         @endforeach
     </ul>
 
+    <div class="mt-8">
+        <h3 class="text-lg font-semibold mb-2">Tu Respuesta...</h3>
+
+        <form action="{{ route('answers.store', $question) }}" method="POST">
+            @csrf
+
+            <div class="mb-2">
+                <textarea name="content" rows="6" class="w-full p-2 border rounded-md text-xs" required></textarea>
+                @error('content')<span class="block text-red-500 text-xs">{{ $message }}</span>@enderror
+            </div>
+
+            <button type="submit" class="rounded-md bg-blue-600 hover:bg-blue-500 px-4 py-2 text-white cursor-pointer">
+                Enviar Respuesta
+            </button>
+        </form>
+    </div>
 </x-forum.layouts.app>
