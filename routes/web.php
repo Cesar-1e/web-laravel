242e05d0f162f588599dea53a6914c44cae6d729
Cesar 1e <cesar.augusto9782@gmail.com>
2025-12-16 13:47:18 -0500

    Implementación de formularios de creación con validación en Laravel

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,12 @@ use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
-Route::get('questions', [QuestionController::class, 'index'])->name('questions.index');
 Route::get('blogs', [BlogController::class, 'index'])->name('blogs.index');
+Route::get('questions', [QuestionController::class, 'index'])->name('questions.index');
+
+Route::get('questions/create', [QuestionController::class, 'create'])->name('questions.create');
+Route::post('questions', [QuestionController::class, 'store'])->name('questions.store');
+
 Route::get('questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
 Route::delete('questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
 
