0a887dc998c79f60ee01a0d20eec321b3216c1c8
Cesar 1e <cesar.augusto9782@gmail.com>
2026-01-05 19:26:37 -0500

    Creación de formularios de edición con Laravel

--- resources/views/questions/_form.blade.php
+++ resources/views/questions/_form.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-4">
     <label class="text-xs font-medium text-gray-700">Título</label>
-    <input type="text" name="title" class="w-full p-2 border border-gray-700 rounded-md" value="{{ old('title') }}" />
+    <input type="text" name="title" class="w-full p-2 border border-gray-700 rounded-md" value="{{ old('title', $question->title ?? '') }}" />
     
     @error('title')<div class="text-red-500 text-xs">{{ $message }}</div>@enderror
 </div>
@@ -14,7 +14,7 @@
 
         <option 
         value="{{ $category->id }}"
-        @if ($category->id == old('category_id')) selected @endif
+        @if ($category->id == old('category_id', $question->category_id ?? null)) selected @endif
         >{{ $category->name }}</option>
 
         @endforeach
@@ -26,7 +26,7 @@
 
 <div class="mb-4">
     <label class="text-xs font-medium text-gray-700">Descripción</label>
-    <textarea name="description" rows="6" class="w-full p-2 border border-gray-700 rounded-md">{{ old('description') }}</textarea>
+    <textarea name="description" rows="6" class="w-full p-2 border border-gray-700 rounded-md">{{ old('description', $question->description ?? '') }}</textarea>
 
     @error('description')<div class="text-red-500 text-xs">{{ $message }}</div>@enderror                
 </div>
